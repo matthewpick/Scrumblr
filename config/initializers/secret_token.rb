32d99a4d2ec1691c728aabc5aa4b633f110f4a3b
saasbook <saasbook@saasbook.(none)>
2014-10-27 19:50:09 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrumbler::Application.config.secret_token = '838048ec6209e704a0d21e346bebf6c457b9484d90d714ffe0c5f8fa9c3d6b37720c9d7fa91a60a03a96d3b1bd17e348d2781dea15806ec6e278a6007d1e5692'

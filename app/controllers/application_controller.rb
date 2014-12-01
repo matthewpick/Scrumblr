0e1ce4f9e1a707cc9d5f404be4174e99b7e4e75b
david-harshman <david-harshman@uiowa.edu>
2014-11-30 23:03:41 -0400

    helpers for sprint/project form client-side validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  
   def set_current_user
   	# @current_user = User.find(1)
   	#@current_user ||= session[:session_token] && User.find_by_session_token(session[:session_token])

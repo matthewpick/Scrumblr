b3245187c2d85501c3f658baf51d0f5c8d25b9a0
Matthew Pick <mpick92@gmail.com>
2014-12-09 15:53:06 -0600

    Add 'View Profile' page for team members

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Scrumbler::Application.routes.draw do
   match '/tasks/:task_id/moveleft' => 'tasks#move_left', via: :get
   match '/tasks/:task_id/moveright' => 'tasks#move_right', via: :get
   match '/tasks/:task_id' => 'tasks#destroy', via: :delete
+  match '/users/view_profile/:uid' => 'users#view_profile', via: :get
   root :to => redirect('/home')
 end
